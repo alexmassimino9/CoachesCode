4674fc3fb843d601d7e27fe4cff45f7d40beb68a
alexmassimino9 <alexmassimino9@gmail.com>
2023-03-08 01:05:18 -0500

    implement auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "coachescode863e8e79": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
-
-const App = () => {
-  return <main style={styles.main}>Coaches Code Coming Soon...</main>;
-};
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 const styles = {
   main: {
     display: "flex",
@@ -14,4 +19,21 @@ const styles = {
     fontWeight: "bold",
   },
 };
-export default App;
+const App = ({ signOut }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <>
+      {/* <main style={styles.main}>Coaches Code Coming Soon...</main> */}
+      <View className="App">
+        <Card>
+          <Image src={logo} className="App-logo" alt="logo" />
+          <Heading level={1}>We now have Auth!</Heading>
+        </Card>
+        <Button onClick={signOut}>Sign Out</Button>
+      </View>
+    </>
+  );
+};
+
+export default withAuthenticator(App);
