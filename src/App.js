43bc6b4545d1274c940786971161863cdb27a6d1
alexmassimino9 <alexmassimino9@gmail.com>
2023-03-13 01:39:12 -0400

    restructure app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import "@aws-amplify/ui-react/styles.css";
+import "@aws-amplify/ui-react/styles.css";
 import { API, Storage } from "aws-amplify";
 import {
   // Button,
@@ -16,8 +16,88 @@ import {
 //   createNote as createNoteMutation,
 //   deleteNote as deleteNoteMutation,
 // } from "./graphql/mutations";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+const TemporaryDrawer = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setIsMenuOpen((state) => !state);
+  };
+
+  const listItems = [
+    "Inbox",
+    "Starred",
+    "Send email",
+    "Drafts",
+    "All mail",
+    "Trash",
+    "Spam",
+  ];
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer("left", false)}
+      onKeyDown={toggleDrawer("left", false)}
+    >
+      <List>
+        {listItems.map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <>
+        <Button onClick={toggleDrawer("left", true)}>Nav</Button>
+        <Drawer
+          anchor={"left"}
+          open={isMenuOpen}
+          onClose={() => setIsMenuOpen((state) => !state)}
+        >
+          {list()}
+        </Drawer>
+      </>
+    </div>
+  );
+};
 
 const App = ({ signOut }) => {
+  return (
+    <>
+      <Button onClick={signOut}>Sign Out</Button>
+      <TemporaryDrawer />
+    </>
+  );
+
   //
   // const [notes, setNotes] = useState([]);
   // useEffect(() => {
