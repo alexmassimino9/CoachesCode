1aaabf50e9223344093b9b7b66411ff2f6474f1b
alexmassimino9 <alexmassimino9@gmail.com>
2023-03-17 16:16:59 -0400

    homepage ui update

--- src/pages/home/homePageComponents/HeroSection.js
+++ src/pages/home/homePageComponents/HeroSection.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Typography } from "@mui/material";
 
 const HeroSection = () => {
   return (
     <section className="hero">
-      <h1>Welcome to Coach's Code!</h1>
-      <p>The team management app for coaches and players.</p>
+      <Typography variant="h1" component="h1" gutterBottom>
+        Welcome to Coach's Code!
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom>
+        The team management app for coaches and players.
+      </Typography>
     </section>
   );
 };
