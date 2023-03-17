4f074eaa7e4bbde3de24c522aeecb685b09dabfb
alexmassimino9 <alexmassimino9@gmail.com>
2023-03-17 16:08:51 -0400

    update features section

--- src/pages/home/homePageComponents/FeatureSection.js
+++ src/pages/home/homePageComponents/FeatureSection.js
@@ -9,33 +9,44 @@ const FeatureSectionRoot = styled("section")(({ theme }) => ({
   marginTop: theme.spacing(4),
 }));
 
-const FeatureSectionHeader = styled("h2")(({ theme }) => ({
+const FeatureSectionHeader = styled("h1")(({ theme }) => ({
   marginBottom: theme.spacing(2),
+  textAlign: "center",
+}));
+
+const FeatureItem = styled(ListItem)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  border: `2px solid var(--color-secondary)`,
+  borderRadius: "20px",
+  padding: theme.spacing(2),
+  margin: theme.spacing(2),
 }));
 
 const FeatureSection = () => {
   return (
     <FeatureSectionRoot>
       <FeatureSectionHeader>Features</FeatureSectionHeader>
-      <List>
-        <ListItem>
-          <ListItemIcon>
+      <List sx={{ display: "flex", justifyContent: "center" }}>
+        <FeatureItem>
+          <ListItemIcon sx={{ fontSize: "3rem" }}>
             <SportsSoccerIcon />
           </ListItemIcon>
           <ListItemText primary="Create teams and manage players" />
-        </ListItem>
-        <ListItem>
-          <ListItemIcon>
+        </FeatureItem>
+        <FeatureItem>
+          <ListItemIcon sx={{ fontSize: "3rem" }}>
             <GroupIcon />
           </ListItemIcon>
           <ListItemText primary="Join teams and participate in events" />
-        </ListItem>
-        <ListItem>
-          <ListItemIcon>
+        </FeatureItem>
+        <FeatureItem>
+          <ListItemIcon sx={{ fontSize: "3rem" }}>
             <ChatIcon />
           </ListItemIcon>
           <ListItemText primary="Stay connected with in-app chat" />
-        </ListItem>
+        </FeatureItem>
       </List>
     </FeatureSectionRoot>
   );
