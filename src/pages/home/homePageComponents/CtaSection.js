cd5517fdc842b48b5b3a33bad59c1055075cc73f
alexmassimino9 <alexmassimino9@gmail.com>
2023-03-18 13:25:35 -0400

    update CTA section ui

--- src/pages/home/homePageComponents/CtaSection.js
+++ src/pages/home/homePageComponents/CtaSection.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { styled } from "@mui/material/styles";
+import { Box, Button, Typography } from "@mui/material";
+
+const CtaSectionRoot = styled("section")(({ theme }) => ({
+  backgroundColor: "var(--color-secondary)",
+  color: "var(--color-white)",
+  padding: theme.spacing(6),
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const CtaButton = styled(Button)(({ theme }) => ({
+  backgroundColor: "var(--color-white)",
+  color: "var(--color-secondary)",
+  borderRadius: "50px",
+  padding: theme.spacing(2, 6),
+  textTransform: "uppercase",
+  "&:hover": {
+    backgroundColor: "var(--color-primary)",
+    color: "var(--color-white)",
+  },
+}));
 
 const CtaSection = () => {
   return (
-    <section className="cta">
-      <h2>Create teams, manage players, and stay organized.</h2>
-      <button>Get Started</button>
-    </section>
+    <CtaSectionRoot>
+      <Typography variant="h2" align="center">
+        Create teams, manage players, and stay organized.
+      </Typography>
+      <Box marginTop={4}>
+        <CtaButton variant="contained" size="large">
+          Get Started
+        </CtaButton>
+      </Box>
+    </CtaSectionRoot>
   );
 };
 
