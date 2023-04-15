cb66cf5c577f9f612b44d8db6ce2421aa6d91ecf
alexmassimino9 <alexmassimino9@gmail.com>
2023-04-14 22:04:09 -0400

    update profile page

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 import { AppBar, ProfileIcon } from "../../components";
-import UserContext, { UserProvider } from "../../context/UserContext";
+import UserContext from "../../context/UserContext";
 import { ProfileCarousel } from "./profilePageComponents";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
@@ -23,41 +23,39 @@ const ProfilePage = () => {
   return (
     <>
       {userDetails && (
-        <UserProvider>
-          <main className="main">
-            <AppBar />
-            <section className="profileContainer">
-              <aside className="profileAside">
-                {/* Display user details */}
-                <ProfileIcon
-                  img={userDetails?.username?.charAt(0).toUpperCase()}
-                />
-                <Typography variant="h4" component="h2">
-                  {userDetails?.username}
-                </Typography>
-                <Typography variant="h6" component="h3">
-                  {userDetails?.attributes?.email}
-                </Typography>
-                <Typography variant="h6" component="h3">
-                  Team Name
-                </Typography>
+        <main className="main">
+          <AppBar />
+          <section className="profileContainer">
+            <aside className="profileAside">
+              {/* Display user details */}
+              <ProfileIcon
+                img={userDetails?.username?.charAt(0).toUpperCase()}
+              />
+              <Typography variant="h4" component="h2">
+                {userDetails?.username}
+              </Typography>
+              <Typography variant="h6" component="h3">
+                {userDetails?.attributes?.email}
+              </Typography>
+              <Typography variant="h6" component="h3">
+                Team Name
+              </Typography>
 
-                {/* Add edit profile button */}
-                <Button
-                  variant="contained"
-                  color="primary"
-                  sx={{ marginTop: "15px" }}
-                >
-                  Edit Profile
-                </Button>
-              </aside>
-              <div className="profileContentContainer">
-                {/* Display profile carousel */}
-                <ProfileCarousel />
-              </div>
-            </section>
-          </main>
-        </UserProvider>
+              {/* Add edit profile button */}
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{ marginTop: "15px" }}
+              >
+                Edit Profile
+              </Button>
+            </aside>
+            <div className="profileContentContainer">
+              {/* Display profile carousel */}
+              <ProfileCarousel />
+            </div>
+          </section>
+        </main>
       )}
     </>
   );
